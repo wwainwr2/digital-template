d0d6c285f3fcdcb6f0d947f7e087f284024d4c06
wwainwr2 <rosswainwright75@yahoo.com>
2015-03-18 14:04:43 -0400

    game 6

--- js/main.js
+++ js/main.js
@@ -1,39 +1,42 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    "use strict";
+Enemy = function(index,game,player){
+	var x = game.world.randomX;
+	var y = gmae.world.randomY;
+	this.game = game;
+	this.player = player;
+	this.virus =game.add.sprite(x,y,'virus');
+	this.virus.anchor.set(0.5,0.5);
+	game.physics.enable(this.enemy,Phaser.Pysics.ARCADE);
+}
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+        
+        game.load.image( 'background', 'assets/background.png' );
+		game.load.image( 'ship', 'assets/ship.png' );
+		game.load.image( 'virus', 'assests/virus.png' );
     }
     
-    var bouncy;
+    var player;
+	var enemies;
+	var NumberofEnemies = 10;
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+        player = game.add.sprite( game.world.centerX, game.world.centerY, 'ship' );
+ 
+        player.anchor.setTo( 0.5, 0.5 );
         
         // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
+        game.physics.enable( player, Phaser.Physics.ARCADE );
         // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
+        player.body.collideWorldBounds = true;
+        enemies = [];
+		for(var i = 0;i<NumberofEnemies;i++)
+		{
+			enemies.push(new Enemy(i,game,player));
+		}
+		cursors = game.input.keyboard.createCursorKeys();
         // Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
         var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
@@ -42,11 +45,29 @@ window.onload = function() {
     }
     
     function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+        if(cursors.left.isDown)
+		{
+			player.angle -=4;
+		}
+		else if(cursors.right.isDown)
+		{
+			player.angle +=4;
+		}
+		if(cursors.up.isDown)
+		{
+			currentSpeed = 300;
+		}
+		else{
+			if(currentSpeed > 0)
+			{
+				currentSpeed -= 4;
+			}
+		}
+		background.tilePosition.x = -game.camera.x;
+		land.tilePosition.y = -game.camera.y;
+        //bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
     }
+	function playerHitEnemy(player, Enemy){
+		enemies[Enemy.name].kill();
+	}
 };
