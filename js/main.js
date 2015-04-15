f4a9f832ca7d81ddeaaae0d0360911d771ed821b
wwainwr2 <rosswainwright75@yahoo.com>
2015-04-15 13:57:04 -0400

    kk

--- js/main.js
+++ js/main.js
@@ -166,3 +166,10 @@ function PlayerHitStar (player, star) {
 
 }
 
+function accelerateToObject(obj1, obj2, speed) {
+    if (typeof speed === 'undefined') { speed = 60; }
+    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
+    obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
+    obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject 
+    obj1.body.force.y = Math.sin(angle) * speed;
+}
