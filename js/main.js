0dd15431ef6f2ae37aa177d77609fb149c4e8ade
wwainwr2 <rosswainwright75@yahoo.com>
2015-04-15 14:01:42 -0400

    kk

--- js/main.js
+++ js/main.js
@@ -123,7 +123,7 @@ function update() {
 		//enemies.rotation = game.physics.arcade.accelerateToObject(enemies,player,100,100,100);
         
 		
-		enemies.forEachAlive(moveEnemies,this);
+		enemies.forEachAlive(game.physics.moveToObject(enemy,player,100,100,100),this);
 		
         game.physics.arcade.overlap(player, stars, PlayerHitStar, null, this);
 		game.physics.arcade.overlap(player, enemies, PlayerHitEnemy, null, this);
@@ -166,10 +166,3 @@ function PlayerHitStar (player, star) {
 
 }
 
-function accelerateToObject(obj1, obj2, speed) {
-    if (typeof speed === 'undefined') { speed = 60; }
-    var angle = Math.atan2(obj2.y - obj1.y, obj2.x - obj1.x);
-    obj1.body.rotation = angle + game.math.degToRad(90);  // correct angle of angry bullets (depends on the sprite used)
-    obj1.body.force.x = Math.cos(angle) * speed;    // accelerateToObject 
-    obj1.body.force.y = Math.sin(angle) * speed;
-}
